9f8e325d5d39b74b3ccd1bec008e06fdbe8cd6e0
Mikkel Kirkegaard <fisk9889@gmail.com>
2021-03-14 17:31:39 +0100

    ILock and Lock implemented

---
+++ Lock/ELockState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LockSimulator
+{
+    public enum ELockState
+    {
+        LockOpen,
+        LockClosed,
+    };
+}
---
+++ Lock/Lock.cs
@@ -0,0 +1,22 @@
+﻿namespace LockSimulator
+{
+    public class Lock : ILock
+    {
+        public Lock()
+        {
+            LockState = ELockState.LockOpen;
+        }
+
+        public void LockDoor()
+        {
+            LockState = ELockState.LockClosed;
+        }
+
+        public void UnlockDoor()
+        {
+            LockState = ELockState.LockOpen;
+        }
+
+        public ELockState LockState { get; private set; }
+    }
+}
