168599f515eec1fe77397b168063324547d632a9
Bjarke <201805703@post.au.dk>
2021-03-17 12:02:04 +0100

    Some Charger test Added, NOT done

--- Ladeskab.Test.Unit/TestCharger.cs
+++ Ladeskab.Test.Unit/TestCharger.cs
@@ -6,6 +6,7 @@ using NSubstitute;
 using NUnit.Framework;
 using Charger;
 using UsbChageSimulator;
+using Display;
 
 namespace Ladeskab.Test.Unit
 {
@@ -13,15 +14,33 @@ namespace Ladeskab.Test.Unit
     public class TestCharger
     {
         private ICharger _uut;
-        private IUsbCharger _usbCharger;    
+        private IUsbCharger _usbCharger;
+        private UsbChageSimulator.UsbChargerSimulator _usbSimulator;
+        private IDisplay _display;
         [SetUp]
         public void Setup()
         {
+            _display = Substitute.For<IDisplay>();
             _usbCharger = Substitute.For<IUsbCharger>();
-            _uut = new Charger.Charger(_usbCharger);
+            _uut = new Charger.Charger(_usbCharger,_display);
         }
 
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsConnectedReturnsCorrectConnectedStatus(bool expected)
+        {
+            //Arange
+            _usbCharger.Connected.Returns(expected);
 
+            Assert.That(_uut.IsConnected(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void StopChargeCallsStopCharge()
+        {
+            _uut.StopCharge();
+            _usbCharger.Received(1).StopCharge();
+        }
 
     }
 }
