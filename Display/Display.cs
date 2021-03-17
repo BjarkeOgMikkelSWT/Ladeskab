05adb87223fbe7d0d5504d3630a1748da4b8685b
Bjarke <201805703@post.au.dk>
2021-03-17 11:04:46 +0100

    Display added

---
+++ Display/Display.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Display
+{
+    public class Display : IDisplay
+    {
+        public void DisplayString(string inputS)
+        {
+            Console.WriteLine(inputS);
+        }
+    }
+}
---
+++ Ladeskab.Test.Unit/TestDisplay.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+using NUnit.Framework;
+using Display;
+using NSubstitute;
+
+namespace Ladeskab.Test.Unit
+{
+    [TestFixture]
+    public class TestDisplay
+    {
+        private IDisplay _uut;
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new Display.Display();
+        }
+
+        [TestCase("String 1")]
+        [TestCase("String with @3€$€${@£")]
+        [TestCase("")]
+        public void DisplayStringDisplaysOnConsole(string expected)
+        {
+            using var consoleOutput = new ConsoleOutput();
+
+            _uut.DisplayString(expected);
+
+            Assert.That(expected, Is.EqualTo(consoleOutput.GetOuput()));
+        }
+
+        [TestCase("String 1")]
+        [TestCase("String with @3€$€${@£")]
+        [TestCase("")]
+        public void DisplayStringFailsWithWrongInputs(string expected)
+        {
+            using var consoleOutput = new ConsoleOutput();
+
+            _uut.DisplayString(expected + "Kage");
+
+            Assert.That(expected, Is.Not.EqualTo(consoleOutput.GetOuput()));
+        }
+    }
+}
