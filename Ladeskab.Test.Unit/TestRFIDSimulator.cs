0803cd2c7bdcc8cf6be9d2010ab966a2bc69187a
Mikkel Kirkegaard <fisk9889@gmail.com>
2021-03-17 09:35:34 +0100

    Implemented the RFID simulator

---
+++ Ladeskab.Test.Unit/TestRFIDSimulator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RFIDReaderSimulator;
+
+namespace Ladeskab.Test.Unit
+{
+    [TestFixture]
+    public class TestRFIDSimulator
+    {
+
+        private RFIDReaderSimulator.RFIDReaderSimulator _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new RFIDReaderSimulator.RFIDReaderSimulator();
+        }
+
+        [TestCase(42)]
+        [TestCase(0)]
+        [TestCase(-10)]
+        [TestCase(2000)]
+        public void EventReceiverGetsCorrectID(int id)
+        {
+            var ID = int.MinValue;
+            _uut.RFIDReadEvent += (o, args) => ID = args.RFID;
+
+            _uut.SimulateRFIDScan(id);
+
+            Assert.That(ID, Is.EqualTo(id));
+        }
+    }
+}
---
+++ ReaderSimulator/RFIDReaderSimulator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RFIDReaderSimulator
+{
+    public class RFIDReaderSimulator : IRFIDReader
+    {
+        public event EventHandler<RFIDReaderEventArgs> RFIDReadEvent;
+
+        public void SimulateRFIDScan(int id)
+        {
+            OnRFIDRead(id);
+        }
+
+        private void OnRFIDRead(int id)
+        {
+            RFIDReadEvent?.Invoke(this, new RFIDReaderEventArgs() {RFID = id});
+        }
+    }
+}
