9b8bafbe2f7bc26806e3051014edb8c78994635c
Bjarke <201805703@post.au.dk>
2021-03-18 16:39:55 +0100

    Door Added and tested

---
+++ Door/DoorSimulator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Door
+{
+    public class DoorSimulator : IDoor
+    {
+        public event EventHandler<DoorEventArg> DoorChangedEvent;
+
+        public void SimulateDoorChange(DoorStateEnum DoorState)
+        {
+            OnDoorStateChange(DoorState);
+        }
+
+        private void OnDoorStateChange(DoorStateEnum DoorStateIn)
+        {
+            DoorChangedEvent?.Invoke(this, new DoorEventArg() { DoorState = DoorStateIn });
+        }
+    }
+}
---
+++ Ladeskab.Test.Unit/TestDoorSimulator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Door;
+
+
+namespace Ladeskab.Test.Unit
+{
+    [TestFixture]
+    class TestDoorSimulator
+    {
+        private DoorSimulator _uut;
+        private DoorEventArg _reciecedDoorArg;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _reciecedDoorArg = null;
+            _uut = new DoorSimulator();
+            _uut.DoorChangedEvent += (o, args) => _reciecedDoorArg = args;
+        }
+
+        [Test]
+        public void DoorStateEventFired()
+        {
+            _uut.SimulateDoorChange(DoorStateEnum.Open);
+            Assert.That(_reciecedDoorArg, Is.Not.Null);
+        }
+       
+        [TestCase(DoorStateEnum.Closed)]
+        [TestCase(DoorStateEnum.Open)]
+        public void DoorStateIsStoredInArg(DoorStateEnum state)
+        {
+            _uut.SimulateDoorChange(state);
+            Assert.That(state, Is.EqualTo(_reciecedDoorArg.DoorState));
+        }
+
+    }
+}
